bbe611d26a3433a05326a9861f60eaa79a1033e5
DoanNguyen <doannguyen102002@gmail.com>
2023-10-06 10:58:32 +0700

    Database

---
+++ Database/DatabaseConnection.java
@@ -0,0 +1,62 @@
+package Database;
+
+import javax.swing.*;
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final String database = "Book";
+    private static final String user = "root";
+    private static final String password = "123456";
+    private static final String url = "jdbc:mysql://localhost:3306/Book";
+
+    private static Connection instance;
+
+    static {
+        try {
+            instance = DatabaseConnection.getConnection();
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Không thể kết nối đến cơ sở dữ liệu. Hãy chạy lại ứng dụng!",
+                    "Lỗi",
+                    JOptionPane.ERROR_MESSAGE
+            );
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private DatabaseConnection() {
+    }
+
+    private static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+                url,
+                user,
+                password
+        );
+    }
+
+    public static Connection getConnectionInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Connection con;
+        Statement statement;
+        ResultSet resultSet;
+        try {
+            con = DatabaseConnection.getConnectionInstance();
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("select * from Sach where name like '%L_%'");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
